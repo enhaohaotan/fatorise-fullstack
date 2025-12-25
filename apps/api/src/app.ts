27bd6a9f0d1c324ba4108bec71d46911853e42cf
Enhao Tan <enhaohao.tan@gmail.com>
2025-12-25 01:01:41 +0100

    feat(api): add cors middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { healthRouter } from "./routes/health.routes.js";
+import { errroMiddeleware } from "./middlewares/error.middlewares.js";
+import cors from "cors";
 
 export function createApp() {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
 
   app.use("/health", healthRouter);
+
+  app.use(errroMiddeleware);
 
   return app;
 }
